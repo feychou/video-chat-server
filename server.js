d064f755f488b80d1eb026bc56f0ae492d8b25c9
fey <federica.recanatini@gmail.com>
2021-10-01 10:58:05 +0200

    fix indentation ew what was that

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express()
 const server = http.createServer(app)
 
 const io = require("socket.io")(server, {
-	cors: {
-		origin: ["http://localhost:3000", "http://localhost:3001" ],
-		methods: [ "GET", "POST" ]
-	}
+  cors: {
+    origin: ["http://localhost:3000", "http://localhost:3001" ],
+    methods: [ "GET", "POST" ]
+  }
 })
 
 let users = {}
@@ -18,22 +18,22 @@ io.on("connection", (socket) => {
     users[userId] = socket.id
   });
 
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	})
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded")
+  })
 
-	socket.on("callUser", (data) => {
-		io.to(users[data.userToCall]).emit("callUser", { 
+  socket.on("callUser", (data) => {
+    io.to(users[data.userToCall]).emit("callUser", { 
       signal: data.signalData,
       from: data.from,
       name: data.name
     })
-	})
+  })
 
-	socket.on("answerCall", (data) => {
-		io.to(users[data.to]).emit("callAccepted", data.signal)
-	})
-  
+  socket.on("answerCall", (data) => {
+    io.to(users[data.to]).emit("callAccepted", data.signal)
+  })
+
 })
 
 server.listen(5000, () => console.log("server is running on port 5000"))
